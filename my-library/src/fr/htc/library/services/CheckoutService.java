64b425be5f53b5e1dddd7d087bd8133c559cbab4
djamel MOUCHENE <promo02.2020@gmail.com>
2020-08-05 17:20:10 +0200

    Add Service interface for : 1.- Book 2.- Member 3.- Checkout

---
+++ my-library/src/fr/htc/library/services/CheckoutService.java
@@ -0,0 +1,15 @@
+package fr.htc.library.services;
+
+import java.util.List;
+
+import fr.htc.library.data.Book;
+
+public interface CheckoutService {
+	
+	public boolean checkout(String matricule, String cote);
+	
+	public List<Book> getAvailableBooks();
+	
+	public boolean canMemberCheckoutYet(String matricule);
+
+}
